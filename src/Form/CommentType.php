9021dab35457fd404b2ec4a6acce8d4e1aeb04dc
Isabelle Moriamez <lisadev84@gmail.com>
2024-09-13 16:01:11 +0200

    fichier test

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,8 +3,6 @@
 namespace App\Form;
 
 use App\Entity\Comment;
-use App\Entity\Question;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
---
+++ tests/Form/CommentTypeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Form;
+
+use PHPUnit\Framework\TestCase;
+
+class CommentTypeTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
